fce7d1d7e6da1c27f5980ef97645f3a37a9f239a
Kawtar Sahili <sahilikawtar112@gmail.com>
2025-07-31 00:49:21 +0100

    Last version

--- NISATEL/src/App.jsx
+++ NISATEL/src/App.jsx
@@ -46,8 +46,6 @@ function App() {
             <Route path="/solutions-techniques" element={<SolutionsTechniques />} />
           </Routes>
         </main>
-console.log("Langue détectée :", i18n.language);
-console.log("Langue locale stockée :", localStorage.getItem("lang"));
         <Footer />
       </div>
     </Router>
